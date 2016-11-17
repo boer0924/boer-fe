2854f71dcdb7de0a2c817197701d8b6f31fbfcd6
boer <boer0924@gmail.com>
2016-11-17 16:26:38 +0800

    add login page

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $(function () {
     });
     mask.on('click', function () {
         mask.fadeOut();
+        login_cover.fadeOut();
         sidebar.animate({'right': -sidebar.width()})
     });
 
@@ -32,4 +33,23 @@ $(function () {
     
     // Initialize HighlightingJS
     hljs.initHighlightingOnLoad();
+
+    let do_login = $('#do-login');
+    let login_cover = $('.login-cover');
+    let show_login_form = function () {
+        let height = $(window).height();
+        let width = $(window).width();
+        let login_top = (height-login_cover.height())/2;
+        let login_left = (width-login_cover.width())/2;
+        login_cover.css('top', login_top);
+        login_cover.css('left', login_left);        
+    };
+    do_login.on('click', function () {
+        mask.fadeIn();
+        show_login_form();
+        login_cover.fadeIn();
+    });
+    $(window).on('resize', function () {
+        show_login_form();
+    });
 });
